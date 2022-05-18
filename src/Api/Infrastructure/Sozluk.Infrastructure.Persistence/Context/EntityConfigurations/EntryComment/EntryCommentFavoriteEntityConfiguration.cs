9d7015966973b92c81700f6ee742fd2d57365dbd
fuatozerr <fuatozerr23@gmail.com>
2022-05-18 18:01:23 +0300

    adding entry config entity framework

---
+++ src/Api/Infrastructure/Sozluk.Infrastructure.Persistence/Context/EntityConfigurations/EntryComment/EntryCommentFavoriteEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sozluk.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sozluk.Infrastructure.Persistence.Context.EntityConfigurations.EntryComment
+{
+    internal class EntryCommentFavoriteEntityConfiguration : BaseEntityConfiguration<Api.Domain.Models.EntryCommentFavorite>
+    {
+        public override void Configure(EntityTypeBuilder<EntryCommentFavorite> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("entrycommentfavorite", SozlukContext.DEFAULT_SCHEMA);
+            builder.HasOne(i => i.EntryComment).WithMany(i => i.EntryCommentFavorites).HasForeignKey(i => i.EntryCommentId);
+            builder.HasOne(i => i.CreatedUser).WithMany(i => i.EntryCommentFavorites).HasForeignKey(i => i.CreatedById);
+        }
+    }
+}
---
+++ src/Api/Infrastructure/Sozluk.Infrastructure.Persistence/Context/EntityConfigurations/EntryComment/EntryCommentVoteEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sozluk.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sozluk.Infrastructure.Persistence.Context.EntityConfigurations.EntryComment
+{
+    internal class EntryCommentVoteEntityConfiguration : BaseEntityConfiguration<Api.Domain.Models.EntryCommentVote>
+    {
+        public override void Configure(EntityTypeBuilder<EntryCommentVote> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("entrycommentvote", SozlukContext.DEFAULT_SCHEMA);
+            builder.HasOne(i => i.EntryComment).WithMany(i => i.EntryCommentVotes).HasForeignKey(i => i.EntryCommentId);
+        }
+    }
+}
