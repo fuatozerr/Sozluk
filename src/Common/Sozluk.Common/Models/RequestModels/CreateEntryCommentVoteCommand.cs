3d75995ba4c893aa4e8dd296dc983e9cd44c05cb
Fuat <fuatozerr23@gmail.com>
2022-05-25 23:27:53 +0300

    bug fix

--- src/Common/Sozluk.Common/Models/RequestModels/CreateEntryCommentVoteCommand.cs
+++ src/Common/Sozluk.Common/Models/RequestModels/CreateEntryCommentVoteCommand.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using MediatR;
+using Sozluk.Common.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sozluk.Common.Models.RequestModels
+namespace Sozluk.Common.Models
 {
-    internal class CreateEntryCommentVoteCommand
+    public class CreateEntryCommentVoteCommand:IRequest<bool>
     {
+        public Guid EntryCommentId { get; set; }
+
+        public VoteType VoteType { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public CreateEntryCommentVoteCommand()
+        {
+
+        }
+
+        public CreateEntryCommentVoteCommand(Guid entryCommentId, VoteType voteType, Guid createdBy)
+        {
+            EntryCommentId = entryCommentId;
+            VoteType = voteType;
+            CreatedBy = createdBy;
+        }
     }
 }
