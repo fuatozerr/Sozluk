b351515cc5acd3644c0cfc63dc0931591a5ce687
Fuat <fuatozerr23@gmail.com>
2022-06-25 12:23:29 +0300

    adding entry service

---
+++ src/Clients/BlazorWeb/Sozluk.WebApp/Infrastructure/Service/Interfaces/IEntryService.cs
@@ -0,0 +1,18 @@
+﻿using Sozluk.Common.Models.Page;
+using Sozluk.Common.Models.Queries;
+using Sozluk.Common.Models.RequestModels;
+
+namespace Sozluk.WebApp.Infrastructure.Service.Interfaces
+{
+    public interface IEntryService
+    {
+        Task<Guid> CreateEntry(CreateEntryCommand command);
+        Task<Guid> CreateEntryComment(CreateEntryCommentCommand command);
+        Task<List<GetEntriesViewModel>> GetEntires();
+        Task<PagedViewModel<GetEntryCommentsViewModel>> GetEntryComments(Guid entryId, int page, int pageSize);
+        Task<GetEntryDetailViewModel> GetEntryDetail(Guid entryId);
+        Task<PagedViewModel<GetEntryDetailViewModel>> GetMainPageEntries(int page, int pageSize);
+        Task<PagedViewModel<GetEntryDetailViewModel>> GetProfilePageEntries(int page, int pageSize, string userName = null);
+        Task<List<SearchEntryViewModel>> SearchBySubject(string searchText);
+    }
+}
